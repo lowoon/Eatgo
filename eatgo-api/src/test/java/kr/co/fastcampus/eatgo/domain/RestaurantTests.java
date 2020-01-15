7050aade86aaef938e2168e0d6ffb70fcc49989e
lowoon <sangbo0229@gmail.com>
2020-01-15 21:16:13 +0900

    Refactor testcode

--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -1,7 +1,9 @@
 package kr.co.fastcampus.eatgo.domain;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.core.Is.*;
+import static org.assertj.core.api.Assertions.*;
+
+//import static org.hamcrest.MatcherAssert.*;
+//import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +11,14 @@ class RestaurantTests {
 	@Test
 	public void creation() {
 		Restaurant restaurant = new Restaurant("Bob Zip", "");
-		assertThat(restaurant.getName(), is("Bob Zip"));
+		//assertThat(restaurant.getName(), is("Bob Zip"));
+		assertThat(restaurant.getName()).isEqualTo("Bob Zip");
 	}
 
 	@Test
 	public void information() {
 		Restaurant restaurant = new Restaurant("Bob Zip", "Seoul");
-		assertThat(restaurant.getInformation(), is("Bob Zip in Seoul"));
+		//assertThat(restaurant.getInformation(), is("Bob Zip in Seoul"));
+		assertThat(restaurant.getName()).isEqualTo("Bob Zip in Seoul");
 	}
 }
