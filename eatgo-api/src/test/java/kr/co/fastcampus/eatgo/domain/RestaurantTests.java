3f38d918f9ac45ed81c0cc98646079b1625d2459
SangBoLee <sangbo0229@gmail.com>
2020-01-15 13:27:47 +0900

    Add

---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,20 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.core.Is.*;
+
+import org.junit.jupiter.api.Test;
+
+class RestaurantTests {
+	@Test
+	public void creation() {
+		Restaurant restaurant = new Restaurant("Bob Zip", "");
+		assertThat(restaurant.getName(), is("Bob Zip"));
+	}
+
+	@Test
+	public void information() {
+		Restaurant restaurant = new Restaurant("Bob Zip", "Seoul");
+		assertThat(restaurant.getInformation(), is("Bob Zip in Seoul"));
+	}
+}
